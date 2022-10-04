8a03f6989e640c3d9d019b2c5fa1b0ded09f019b
Pasises <45708319+pasises@users.noreply.github.com>
2022-10-04 22:48:06 +0900

    feat : calculate localization error

---
+++ calc_rmse.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def poses(src: str):
+    data = np.load(src)
+    estimatde_poses = data['estimated_poses'][1:]
+    img_poses = data['real_poses']
+    diff = img_poses[0] - estimatde_poses[0]
+    estimatde_poses += diff
+    return estimatde_poses, img_poses
+
+
+if __name__ == "__main__":
+    data_dir = "./datasets/feature_less_rock_05"
+    estimated_poses, img_poses = poses(f"{data_dir}/vo_result_poses.npz")
+    # fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
+    # ax.plot(estimated_poses[:, 0], estimated_poses[:, 1], estimated_poses[:, 2], '-o', label='Estimated', markersize=2)
+    # ax.plot(img_poses[:, 0], img_poses[:, 1], img_poses[:, 2], '-o', c='#ff7f0e', markersize=2)
+    # ax.set_xlabel("x")
+    # ax.set_ylabel("y")
+    # # ax.set_xlim(4, 8)
+    # # ax.set_ylim(4, 8)
+    # ax.set_zlim(0, 1)
+    # plt.show()
+
+    rmses = []
+    for e_pos, r_pos in zip(estimated_poses, img_poses):
+        rmses.append(np.linalg.norm(e_pos - r_pos))
+
+    fig, ax = plt.subplots()
+    ax.plot(np.arange(len(rmses)), rmses)
+    ax.set_xlabel("Image Index")
+    ax.set_ylabel("RMSE [m]")
+    plt.show()
+    fig.savefig(f"{data_dir}/rmse.png", dpi=300, bbox_inches='tight', pad_inches=0)
