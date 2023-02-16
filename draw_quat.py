e776bf997cbe8e34820110a988f7977188d1aed4
motthi <45708319+pasises@users.noreply.github.com>
2023-02-16 23:20:27 +0900

    refact

--- draw_quat.py
+++ draw_quat.py
@@ -1,11 +1,11 @@
-from vo.draw import draw_vo_quats
+from vo.draw import draw_vo_poses_and_quats
 from vo.utils import load_result_poses
 
 if __name__ == "__main__":
     data_dir = "./datasets/aki_20221117_1/"
     # data_dir = "./datasets/feature_rich_rock/"
     estimated_poses, estimated_quats, real_poses, real_quats, real_img_poses, real_img_quats = load_result_poses(f"{data_dir}vo_result_poses.npz")
-    draw_vo_quats(
+    draw_vo_poses_and_quats(
         estimated_poses, estimated_quats, real_poses, real_img_poses, real_img_quats,
         draw_data="all",
         view=(-55, 145, -60),
