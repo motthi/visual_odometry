b6655adb8abf546a0e40ab19fe9d5c5ba5b023cc
Motthi <k11000660@gmail.com>
2023-05-23 15:20:27 +0900

    refact : dataset directory

--- visualize/draw_quat.py
+++ visualize/draw_quat.py
@@ -1,11 +1,14 @@
 import sys
 sys.path.append("../")
+import os
 from vo.draw import draw_vo_poses_and_quats
 from vo.utils import load_result_poses
 
+DATASET_DIR = os.environ['DATASET_DIR']
+
 if __name__ == "__main__":
-    data_dir = "../datasets/aki_20230227_1/"
-    estimated_poses, estimated_quats, real_poses, real_quats, real_img_poses, real_img_quats = load_result_poses(f"{data_dir}vo_result_poses.npz")
+    data_dir = f"{DATASET_DIR}/AKI/aki_20230227_1"
+    estimated_poses, estimated_quats, real_poses, real_quats, real_img_poses, real_img_quats = load_result_poses(f"{data_dir}/vo_result_poses.npz")
     draw_vo_poses_and_quats(
         estimated_poses, estimated_quats, real_poses, real_img_poses, real_img_quats,
         draw_data="all",
@@ -13,5 +16,5 @@ if __name__ == "__main__":
         # xlim=(-2.0, 2.0),
         ylim=(0.0, 1.0),
         # zlim=(0, 1),
-        save_src=f"{data_dir}quats_result.png"
+        save_src=f"{data_dir}/quats_result.png"
     )
