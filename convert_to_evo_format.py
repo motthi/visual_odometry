e5b1a5a63bd74dd38a93133e4e891d511fa7431c
Motthi <k11000660@gmail.com>
2023-06-22 17:56:16 +0900

    feat : refact

--- convert_to_evo_format.py
+++ convert_to_evo_format.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     dataset_dir = f"{DATASET_DIR}/AKI/aki_20230227_2"
     dataset = AkiDataset(dataset_dir)
 
-    # dataset_dir = f"{DATASET_DIR}/MADMAX/LocationA/A-0"
-    # dataset = MadmaxDataset(dataset_dir)
+    dataset_dir = f"{DATASET_DIR}/MADMAX/LocationA/A-0"
+    dataset = MadmaxDataset(dataset_dir)
 
     print(f"Dataset directory: {dataset_dir}")
-    gt_poses, gt_quats = dataset.read_all_poses_quats()
+    gt_ts, gt_poses, gt_quats = dataset.read_all_poses_quats()
 
     # save_trajectory(f"{dataset_dir}/gt_traj.kitti_f", None, gt_poses, gt_quats, 'kitti')
-    save_trajectory(f"{dataset_dir}/gt_traj.tum_f", gt_poses, gt_quats, 'tum')
+    save_trajectory(f"{dataset_dir}/gt_traj.tum_f", gt_ts, gt_poses, gt_quats, 'tum')
