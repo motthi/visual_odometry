4e1a2a067fdffb6bd17a137a2d2965b4b7a8f353
Motthi <k11000660@gmail.com>
2023-07-21 11:08:52 +0900

    feat : correct distortion

--- vo/datasets/__init__.py
+++ vo/datasets/__init__.py
@@ -1,7 +1,17 @@
 import cv2
 import os
 import numbers
+import numpy as np
 from tqdm import tqdm
+
+
+def distortion_correction(img: np.ndarray, K: np.ndarray, D: np.ndarray) -> np.ndarray:
+    h, w = img.shape[:2]
+    nmat, roi = cv2.getOptimalNewCameraMatrix(K, D, (w, h), 1, (w, h))
+    dst = cv2.undistort(img, K, D, None, nmat)
+    x, y, w, h = roi
+    dst = dst[y:y + h, x:x + w]
+    return dst
 
 
 class ImageDataset():
@@ -16,13 +26,21 @@ class ImageDataset():
         self.step = step
         self.l_img_srcs = []
         self.r_img_srcs = []
+        self.lcam_params = {}
+        self.rcam_params = {}
 
     def load_imgs(self):
         l_imgs, r_imgs = [], []
         print("Loading images...")
         for limg_src, rimg_src in zip(tqdm(self.l_img_srcs), self.r_img_srcs):
-            l_imgs.append(cv2.imread(limg_src))
-            r_imgs.append(cv2.imread(rimg_src))
+            if 'distortion' in self.lcam_params.keys() and 'distortion' in self.rcam_params.keys():
+                l_img = distortion_correction(cv2.imread(limg_src), self.lcam_params['intrinsic'], self.lcam_params['distortion'])
+                r_img = distortion_correction(cv2.imread(rimg_src), self.rcam_params['intrinsic'], self.rcam_params['distortion'])
+            else:
+                l_img = cv2.imread(limg_src)
+                r_img = cv2.imread(rimg_src)
+            l_imgs.append(l_img)
+            r_imgs.append(r_img)
         return l_imgs, r_imgs
 
     def pose_quat_slice(self, timestamps, poses, quats, start, last, step):
