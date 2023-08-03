8bc4446b2149862b7e191b403faed3b05523b378
Motthi <k11000660@gmail.com>
2023-08-03 18:09:38 +0900

    refact : message

--- convert_to_evo_format.py
+++ convert_to_evo_format.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     dataset_dir = f"{DATASET_DIR}/AKI/aki_20230227_2"
     dataset = AkiDataset(dataset_dir)
 
-    dataset_dir = f"{DATASET_DIR}/MADMAX/LocationA/A-0"
+    dataset_dir = f"{DATASET_DIR}/MADMAX/LocationD/D-0"
     dataset = MadmaxDataset(dataset_dir)
 
     print(f"Dataset directory: {dataset_dir}")
@@ -17,3 +17,5 @@ if __name__ == "__main__":
 
     # save_trajectory(f"{dataset_dir}/gt_traj.kitti_f", None, gt_poses, gt_quats, 'kitti')
     save_trajectory(f"{dataset_dir}/gt_traj.tum_f", gt_ts, gt_poses, gt_quats, 'tum')
+
+    print(f"Saved ground truth trajectory to {dataset_dir}/gt_traj.tum_f")
