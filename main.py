cb029e092a4817196de5b2e4e3d73987e3ed8b81
Motthi <k11000660@gmail.com>
2023-08-09 16:04:25 +0900

    feat

--- main.py
+++ main.py
@@ -15,18 +15,18 @@ DATASET_DIR = os.environ['DATASET_DIR']
 
 if __name__ == "__main__":
     # Specify the range of images to use
-    start = 0
-    last = None
-    step = 3
-    start = 700
+    # start = 0
+    # last = None
+    # step = 1
+    start = 2000
     last = 4000
-    step = 3
+    step = 7
 
     # Load datasets
     # data_dir = f"{DATASET_DIR}/AKI/aki_20230615_1"
     # dataset = AkiDataset(data_dir, start=start, last=last, step=step)
 
-    data_dir = f"{DATASET_DIR}/MADMAX/LocationA/A-0"
+    data_dir = f"{DATASET_DIR}/MADMAX/LocationD/D-0"
     dataset = MadmaxDataset(data_dir, start=start, last=last, step=step)
 
     save_dir = f"{data_dir}/vo_results/normal"
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 
     lcam_params, rcam_params = dataset.camera_params()
     l_imgs, r_imgs = dataset.load_imgs()
-    _, all_poses, all_quats = dataset.read_all_poses_quats()
+    gt_timestamps, gt_poses, gt_quats = dataset.read_all_poses_quats()
     cap_timestamps, cap_poses, cap_quats = dataset.read_captured_poses_quats()
     num_img = len(l_imgs)
 
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     # detector = ShiTomashiCornerDetector()
     detector = cv2.ORB_create()
     # detector = cv2.AKAZE_create()
-    # detector = BucketingDetector(8, 10, detector, cv2.ORB_create())
+    # detector = BucketingDetector(8, 100, detector, cv2.ORB_create())
 
     # Feature descriptor
     descriptor = cv2.ORB_create()
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     # Tracker
     tracker = BruteForceTracker(cv2.NORM_HAMMING, cross_check=True)
     # tracker = FlannTracker()
-    tracker = OpticalFlowTracker(win_size=(100, 100))
+    # tracker = OpticalFlowTracker(win_size=(100, 100))
 
     # Estimator
     # estimator = MonocularVoEstimator(lcam_params['intrinsic'])
@@ -99,15 +99,16 @@ if __name__ == "__main__":
     vo.save_results(dataset.last, dataset.start, dataset.step, f"{save_dir}/npz")
     vo.estimator.save_results(f"{save_dir}/estimator_result.npz")
     save_trajectory(f"{save_dir}/estimated_trajectory.txt", cap_timestamps, estimated_poses, estimated_quats)
+    save_trajectory(f"{save_dir}/gt_traj.txt", gt_timestamps, gt_poses, gt_quats, 'tum')
     np.savez(
         f"{save_dir}/vo_result_poses.npz",
         estimated_poses=estimated_poses, estimated_quats=estimated_quats,
-        real_poses=all_poses, real_quats=all_quats,
+        real_poses=gt_poses, real_quats=gt_quats,
         real_img_poses=cap_poses, real_img_quats=cap_quats,
         start_idx=dataset.start, last_idx=dataset.last, step=dataset.step
     )
     draw_vo_poses(
-        estimated_poses, all_poses, cap_poses,
+        estimated_poses, gt_poses, cap_poses,
         # view=(-55, 145, -60),
         # ylim=(0.0, 1.0)
     )
