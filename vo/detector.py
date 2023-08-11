b16f9b14ce4879e8cc16e14393bef06da6c011da
motthi <45708319+pasises@users.noreply.github.com>
2023-08-11 16:53:57 +0900

    feat : delete bucketing detector

--- vo/detector.py
+++ vo/detector.py
@@ -34,32 +34,3 @@ class ShiTomashiCornerDetector():
             return []
         kpts = [cv2.KeyPoint(float(x[0][0]), float(x[0][1]), 13) for x in kpts]
         return kpts
-
-
-class BucketingDetector():
-    def __init__(self, num_backets, num_kpts, base_detector):
-        self.num_backets = num_backets
-        self.num_kpts = num_kpts
-        self.detector = base_detector
-
-    def detect(self, img: np.ndarray, mask: np.ndarray = None) -> list[cv2.KeyPoint]:
-        h, w = img.shape[:2]
-        h_step = h // self.num_backets
-        w_step = w // self.num_backets
-        kpts = []
-        for i in range(self.num_backets):
-            for j in range(self.num_backets):
-                kpts_ = self.detector.detect(
-                    img[i * h_step:(i + 1) * h_step, j * w_step:(j + 1) * w_step],
-                    mask[i * h_step:(i + 1) * h_step, j * w_step:(j + 1) * w_step]
-                )
-                if len(kpts_) == 0:
-                    continue
-
-                for pt in kpts_:
-                    pt.pt = (pt.pt[0] + j * w_step, pt.pt[1] + i * h_step)
-
-                kpts_ = list(kpts_)
-                np.random.shuffle(kpts_)
-                kpts += kpts_[:self.num_kpts]
-        return kpts
