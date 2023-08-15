34fad2ce7e514f929cea506d8addaf9f43f7bde7
motthi <45708319+pasises@users.noreply.github.com>
2023-08-16 01:30:08 +0900

    feat : analysis error

---
+++ visualize/rpy_error.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
+from vo.utils import load_result_poses
+
+DATASET_DIR = os.environ['DATASET_DIR']
+
+if __name__ == "__main__":
+    data_dir = f"{DATASET_DIR}/AKI/aki_20230615_1"
+    # data_dir = f"{DATASET_DIR}/MADMAX/LocationA/A-0"
+    result_dir = f"{data_dir}/vo_results/normal"
+    est_poses, _, gt_img_poses = load_result_poses(f"{result_dir}/vo_result_poses.npz")
+    
+    est_rpys = R.from_matrix(est_poses[:, :3, :3]).as_euler("ZXY", degrees=True)
+    gt_img_rpys = R.from_matrix(gt_img_poses[:, :3, :3]).as_euler("ZXY", degrees=True)
+    errors = est_rpys - gt_img_rpys
+
+    idx = errors > 180
+    errors[idx] = errors[idx] - 360
+    idx = errors < -180
+    errors[idx] = errors[idx] + 360
+
+    error_mean = np.mean(errors, axis=0)
+    error_std = np.std(errors, axis=0)
+    print("Rotation errors")
+    print(f"\tRoll\t: {error_mean[0]} +/- {error_std[0]} [deg]")
+    print(f"\tPitch\t: {error_mean[1]} +/- {error_std[1]} [deg]")
+    print(f"\tYaw\t: {error_mean[2]} +/- {error_std[2]} [deg]")
+
+    fig, ax = plt.subplots(3, 1, figsize=(10, 10), sharex=True)
+    ax[0].plot(errors[:, 0])
+    ax[1].plot(errors[:, 1])
+    ax[2].plot(errors[:, 2])
+    ax[0].set_ylabel("Roll [deg]")
+    ax[1].set_ylabel("Pitch [deg]")
+    ax[2].set_ylabel("Yaw [deg]")
+    ax[2].set_xlabel("Frame index")
+    plt.show()
+    fig.savefig(f"{result_dir}/absolute_rot_error.png", dpi=300, bbox_inches='tight', pad_inches=0.1)
+    
+    # draw_rpy_diff(est_poses, gt_img_poses, f"{result_dir}/rpy_diff.png")
+
---
+++ visualize/trans_error.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from vo.utils import load_result_poses
+
+DATASET_DIR = os.environ['DATASET_DIR']
+
+if __name__ == "__main__":
+    data_dir = f"{DATASET_DIR}/AKI/aki_20230615_1"
+    # data_dir = f"{DATASET_DIR}/MADMAX/LocationA/A-0"
+    result_dir = f"{data_dir}/vo_results/normal"
+    est_poses, _, gt_img_poses = load_result_poses(f"{result_dir}/vo_result_poses.npz")
+    est_trans = est_poses[:, :3, 3]
+    gt_img_trans = gt_img_poses[:, :3, 3]
+    trans_errors = est_trans - gt_img_trans
+
+    error_mean = np.mean(trans_errors, axis=0)
+    error_std = np.std(trans_errors, axis=0)
+    print("Translation errors")
+    print(f"\t{error_mean[0]} +/- {error_std[0]} [m]")
+    print(f"\t{error_mean[1]} +/- {error_std[1]} [m]")
+    print(f"\t{error_mean[2]} +/- {error_std[2]} [m]")
+
+    fig, ax = plt.subplots(3, 1, figsize=(10, 10), sharex=True)
+    ax[0].plot(trans_errors[:, 0])
+    ax[1].plot(trans_errors[:, 1])
+    ax[2].plot(trans_errors[:, 2])
+    ax[0].set_ylabel("x [m]")
+    ax[1].set_ylabel("y [m]")
+    ax[2].set_ylabel("z [m]")
+    ax[2].set_xlabel("Frame index")
+    plt.show()
+    fig.savefig(f"{result_dir}/absolute_trans_error.png", dpi=300, bbox_inches='tight', pad_inches=0.1)
+
+    # draw_trans_diff(est_poses, gt_img_poses, f"{result_dir}/trans_diff.png")
