49d2aaa74faf7fbd0c9d37997f47510d48121e99
Motthi <k11000660@gmail.com>
2023-12-22 20:32:24 +0900

    refact : madmax dataset

--- vo/datasets/madmax.py
+++ vo/datasets/madmax.py
@@ -36,74 +36,50 @@ class MadmaxDataset(ImageDataset):
         with open(f"{self.dataset_dir}/../calibration/tf__camera_left_to_camera_right.csv") as f:
             tf_lcam2rcam = f.readlines()
 
-        rot_imu2base = R.from_quat(list(map(float, tf_imu2base[1].strip().split(',')[3:]))).as_matrix()
-        trans_imu2base = np.array([list(map(float, tf_imu2base[1].strip().split(',')[:3]))])
-        T_imu2base = np.eye(4)
-        T_imu2base[:3, :3] = rot_imu2base
-        T_imu2base[:3, 3] = trans_imu2base
-        T_base2imu = np.linalg.inv(T_imu2base)
+        T_imu2base = self.tf2T(tf_imu2base)
+        T_imu2lcam = self.tf2T(tf_imu2lcam)
+        T_lcam2rcam = self.tf2T(tf_lcam2rcam)
 
-        rot_imu2lcam = R.from_quat(list(map(float, tf_imu2lcam[1].strip().split(',')[3:]))).as_matrix()
-        trans_imu2lcam = np.array([list(map(float, tf_imu2lcam[1].strip().split(',')[:3]))])
-        T_imu2lcam = np.eye(4)
-        T_imu2lcam[:3, :3] = rot_imu2lcam
-        T_imu2lcam[:3, 3] = trans_imu2lcam
-
-        rot_lcam2rcam = R.from_quat(list(map(float, tf_lcam2rcam[1].strip().split(',')[3:]))).as_matrix()
-        trans_lcam2rcam = np.array([list(map(float, tf_lcam2rcam[1].strip().split(',')[:3]))])
-        T_lcam2rcam = np.eye(4)
-        T_lcam2rcam[:3, :3] = rot_lcam2rcam
-        T_lcam2rcam[:3, 3] = trans_lcam2rcam
-
-        T_base2lcam = T_base2imu @ T_imu2lcam
+        T_base2lcam = T_imu2base @ T_imu2lcam
         T_base2rcam = T_base2lcam @ T_lcam2rcam
-
-        # T_base2lcam = np.linalg.inv(T_base2lcam)
-        # T_base2rcam = np.linalg.inv(T_base2rcam)
+        # T_imu2rcam = T_imu2lcam @ T_lcam2rcam
 
         # K_l = np.array(lcam_info['K']).reshape(3, 3)
         # K_r = np.array(rcam_info['K']).reshape(3, 3)
         K_l = np.array([lcam_info['P']]).reshape(3, 4)[:, :3]
         K_r = np.array([rcam_info['P']]).reshape(3, 4)[:, :3]
-        E_l = T_base2lcam[:3, :]
-        E_r = T_base2rcam[:3, :]
-        # P_l = K_l @ E_l
-        # P_r = K_r @ E_r
-        P_l = np.array([lcam_info['P']]).reshape(3, 4)  # You need to align estimated trajectory if you use this
-        P_r = np.array([rcam_info['P']]).reshape(3, 4)
-        D_l = np.array(lcam_info['D'])
-        D_r = np.array(rcam_info['D'])
+        E_l = np.linalg.inv(T_base2lcam)[:3, :]
+        E_r = np.linalg.inv(T_base2rcam)[:3, :]
+        P_l = K_l @ E_l
+        P_r = K_r @ E_r
+        # P_l = np.array([lcam_info['P']]).reshape(3, 4)  # You need to align estimated trajectory if you use this
+        # P_r = np.array([rcam_info['P']]).reshape(3, 4)
 
         self.lcam_params = {
             'intrinsic': K_l,
             'extrinsic': E_l,
             'projection': P_l,
-            'distortion': D_l
+            'distortion': np.array(lcam_info['D'])
         }
         self.rcam_params = {
             'intrinsic': K_r,
             'extrinsic': E_r,
             'projection': P_r,
-            'distortion': D_r
+            'distortion': np.array(rcam_info['D'])
         }
         return self.lcam_params, self.rcam_params
 
+    def tf2T(self, tf):
+        rot = R.from_quat(list(map(float, tf[1].strip().split(',')[3:]))).as_matrix()
+        trans = np.array([list(map(float, tf[1].strip().split(',')[:3]))])
+        T = np.eye(4)
+        T[:3, :3] = rot
+        T[:3, 3] = trans
+        return T
+
     def read_captured_poses_quats(self) -> list[np.ndarray]:
-        poses = {}
-        ts = []
-        # with open(f"{self.dataset_dir}/ground_truth/gt_5DoF_gnss.csv") as f:
-        #     lines = f.readlines()
-        # for line in lines[14:]:
-        #     data = line.split(",")
-        #     poses[f'{data[0]}'] = [float(data[3]), float(data[4]), float(data[5]), float(data[10]), float(data[11]), float(data[12]), float(data[9])]
-        #     ts.append(float(data[0]))
-        with open(f"{self.dataset_dir}/ground_truth/gt_6DoF_gnss_and_imu.csv") as f:
-            lines = f.readlines()
-        for line in lines[14:]:
-            data = line.split(",")
-            ts.append(float(data[0]))
-            poses[f'{data[0]}'] = [float(data[1]), float(data[2]), float(data[3]), float(data[8]), float(data[9]), float(data[10]), float(data[7])]
-        ts = np.array(ts, float)
+        # ts, _, _, poses = self.read_5dof_gnss()
+        ts, _, _, poses = self.read_6dof_gnss_imu()
 
         timestamps = []
         trans = []
@@ -121,27 +97,8 @@ class MadmaxDataset(ImageDataset):
         return timestamps, trans, quats
 
     def read_all_poses_quats(self) -> list[np.ndarray]:
-        timestamps = []
-        trans = []
-        quats = []
-        # with open(f"{self.dataset_dir}/ground_truth/gt_5DoF_gnss.csv") as f:
-        #     lines = f.readlines()
-        # for line in lines[14:]:
-        #     data = line.split(",")
-        #     timestamps.append(float(data[0]))
-        #     trans.append([float(data[3]), float(data[4]), float(data[5])])
-        #     quats.append([float(data[10]), float(data[11]), float(data[12]), float(data[9])])   # x, y, z, w
-        with open(f"{self.dataset_dir}/ground_truth/gt_6DoF_gnss_and_imu.csv") as f:
-            lines = f.readlines()
-        for line in lines[14:]:
-            data = line.split(",")
-            timestamps.append(float(data[0]))
-            trans.append([float(data[1]), float(data[2]), float(data[3])])
-            quats.append([float(data[8]), float(data[9]), float(data[10]), float(data[7])])   # x, y, z, w
-        timestamps = np.array(timestamps, float)
-        trans = np.array(trans, dtype=np.float32)
-        quats = np.array(quats, dtype=np.float32)
-
+        # timestamps, trans, quats, _ = self.read_5dof_gnss()
+        timestamps, trans, quats, _ = self.read_6dof_gnss_imu()
         s_img_scr, e_img_scr = self.l_img_srcs[0], self.l_img_srcs[-1]
         s_img_ts = float(img_timestamp_re.search(s_img_scr).group(1)) * 1e-9
         e_img_ts = float(img_timestamp_re.search(e_img_scr).group(1)) * 1e-9
@@ -151,3 +108,39 @@ class MadmaxDataset(ImageDataset):
         trans = trans[start_idx:end_idx + 1]
         quats = quats[start_idx:end_idx + 1]
         return timestamps, trans, quats
+
+    def read_5dof_gnss(self):
+        ts = []
+        t = []
+        q = []
+        poses = {}
+        with open(f"{self.dataset_dir}/ground_truth/gt_5DoF_gnss.csv") as f:
+            lines = f.readlines()
+        for line in lines[14:]:
+            data = line.split(",")
+            ts.append(float(data[0]))
+            t.append([float(data[3]), float(data[4]), float(data[5])])
+            q.append([float(data[10]), float(data[11]), float(data[12]), float(data[9])])   # x, y, z, w
+            poses[f'{data[0]}'] = [float(data[3]), float(data[4]), float(data[5]), float(data[10]), float(data[11]), float(data[12]), float(data[9])]
+        ts = np.array(ts, float)
+        t = np.array(t, dtype=np.float32)
+        q = np.array(q, dtype=np.float32)
+        return ts, t, q, poses
+
+    def read_6dof_gnss_imu(self):
+        ts = []
+        t = []
+        q = []
+        poses = {}
+        with open(f"{self.dataset_dir}/ground_truth/gt_6DoF_gnss_and_imu.csv") as f:
+            lines = f.readlines()
+        for line in lines[14:]:
+            data = line.split(",")
+            ts.append(float(data[0]))
+            t.append([float(data[1]), float(data[2]), float(data[3])])
+            q.append([float(data[8]), float(data[9]), float(data[10]), float(data[7])])   # x, y, z, w
+            poses[f'{data[0]}'] = [float(data[1]), float(data[2]), float(data[3]), float(data[8]), float(data[9]), float(data[10]), float(data[7])]
+        ts = np.array(ts, float)
+        t = np.array(t, dtype=np.float32)
+        q = np.array(q, dtype=np.float32)
+        return ts, t, q, poses
